d38f04def75bce52fd13920dd2847d6dfa89b6d4
Chris Cope <ccope@resilientscience.com>
2014-06-18 13:59:44 -0400

    Cleaned up the dimer code a bit and gave it arbitrary-length wordcounting

--- wot/dimer.py
+++ wot/dimer.py
@@ -3,26 +3,27 @@
 
 import sys
 import fileinput
+import itertools
+
+ALPHABET = ("A", "C", "G", "T")
 
 
-ALPHABET = ["A", "C", "G", "T"]
+def sequence_count(string, wordlength):
+    """Return a frequency count of all sequences of a given size found in a string.
+    
+    
+    """
+    i = 0
+    wc = {}
+    while i < len(string) - wordlength + 1:
+        word = string[i:i+wordlength]
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1
+        i += 1
+    return wc
 
-
-def sequence_count(lines, wordlength):
-    wordcount = {}
-    c0 = None
-    for line in lines:
-        for c in line:
-            if not c0:
-                c0 = c
-            else:
-                word = c0 + c
-                c0 = c
-                if word in wordcount:
-                    wordcount[word] += 1
-                else:
-                    wordcount[word] = 1
-    return wordcount
 
 
 def pretty_print(wordcount_dict, wordlength):
@@ -30,7 +31,8 @@ def pretty_print(wordcount_dict, wordlength):
     """
     output = ""
     e = 0
-    for w in [j+i for j in ALPHABET for i in ALPHABET]:
+    for w in itertools.product(ALPHABET, repeat=wordlength):
+        w = ''.join(w)
         try:
             output += "%3.10s%13s\n" % (e, wordcount_dict[w])
         except IndexError:
@@ -38,12 +40,20 @@ def pretty_print(wordcount_dict, wordlength):
         e += 1
     return output
 
-def histogram(filename):
-    wc = sequence_count(open(filename).readlines(), 2)
-    return pretty_print(wc, 2)    
+def histogram(filename, wordlength):
+    """Returns a histogram report that meets NIHCC's format.
+
+    This function returns a string that is compatible with the example code
+    used Jim Deleo's Scientific Computing group at the NIH Clinical Center.
+    'Pick a standard, any standard', we'll we've agreed to use this.
+    """
+    wc = {}
+    for line in open(filename).readlines():
+        wcnew = sequence_count(line, wordlength)
+        wc = {i: wc.get(i, 0) + wcnew.get(i, 0) for i in set(wc) | set(wcnew)}
+    return pretty_print(wc, wordlength)
+
     
 if __name__ == "__main__":
-    wc = sequence_count(fileinput.input(filename), 2)
-    print pretty_print(wc, 2)    
-
+    print histogram(sys.argv[1], 3)
 
